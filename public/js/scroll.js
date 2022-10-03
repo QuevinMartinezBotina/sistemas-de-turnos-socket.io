af43e8ba067bf27253be425597dd7e37f6d3dd4e
quevinmartinezbotina <quevinmartinezbotina@gmail.com>
2022-10-02 19:13:45 -0500

    added animations and modifications maquetation

---
+++ public/js/scroll.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal();
+
+const animar = (clase, duracion, origen, distancia) => {
+  sr.reveal(clase, {
+    duration: duracion,
+
+    origin: origen,
+
+    distance: distancia,
+  });
+};
+
+animar(".animation-bottom", 1000, "bottom", "-100px");
+
+animar(".animation-top", 2000, "top", "-100px");
+
+animar(".animation-right", 2000, "right", "-100px");
+
+animar(".animation-left", 2000, "left", "-100px");
+
+animar(".menu", 3000, "left", "-100px");
